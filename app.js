a4da6e941cf2a9f1f6a578a12c53dc8690295d7e
cfsdev027 <cfsdev.github@gmail.com>
2026-05-06 19:24:35 -0300

    Update supabase client initialization to use window

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = "https://sdakvoeythnbfqfgupzf.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYWt2b2V5dGhuYmZxZmd1cHpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDA1MjksImV4cCI6MjA5MzY3NjUyOX0.U13KCcwWUTStp1-k8at9CudflI66uJ8YhMzSErQAlrM";
 
 // Inicializa o cliente global do Supabase
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const NOME_COOKIE = "SRP-Mockup-Autentication";
 
