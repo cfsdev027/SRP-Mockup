4cf57c4377ec018e9e307000d5e5216aeca107d6
cfsdev027 <cfsdev.github@gmail.com>
2026-05-06 19:35:02 -0300

    Rename supabase variable and update query table

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const SUPABASE_URL = "https://sdakvoeythnbfqfgupzf.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYWt2b2V5dGhuYmZxZmd1cHpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDA1MjksImV4cCI6MjA5MzY3NjUyOX0.U13KCcwWUTStp1-k8at9CudflI66uJ8YhMzSErQAlrM";
 
 // Inicializa o cliente global do Supabase
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 const NOME_COOKIE = "SRP-Mockup-Autentication";
 
@@ -50,8 +50,8 @@ const ServicoAutenticacao = {
     login: async function(usuario, senha) {
         try {
             // Consulta o Supabase procurando pelo username, se ele está ativo e se a senha bate
-            const { data, error } = await supabase
-                .from('usuarios')
+            const { data, error } = await supabaseClient
+                .from('users')
                 .select('username, ativo')
                 .eq('username', usuario)
                 .eq('password', senha) // Verificação simples de texto puro para o mockup
