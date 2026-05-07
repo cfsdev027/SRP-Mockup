d13600c1196a29a2f93bea0669bd8421b4cc7de5
cfsdev027 <cfsdev.github@gmail.com>
2026-05-07 14:43:17 -0300

    Refactor activate method to use 'this' context

--- app_pages.js
+++ app_pages.js
@@ -8,26 +8,27 @@ export const AppPages = {
     pageLogin: document.getElementById(PAGE_LOGIN_ID),
     pagePonto: document.getElementById(PAGE_PONTO_ID),
     activate: function(page, callback) {
-        pageLogin.classList.remove('ativa');
-        pagePonto.classList.remove('ativa');
+        // Use "this." para referenciar as propriedades do objeto
+        this.pageLogin.classList.remove('ativa');
+        this.pagePonto.classList.remove('ativa');
 
         var pageActive = null;
         switch(page){
           case 'login':
-            pageLogin.classList.add('ativa');
-            pageActive = pageLogin;
+            this.pageLogin.classList.add('ativa');
+            pageActive = this.pageLogin;
             InterfaceLogin.initialize();
             break;
           case 'ponto':
-            pagePonto.classList.add('ativa');
-            pageActive = pagePonto;
+            this.pagePonto.classList.add('ativa');
+            pageActive = this.pagePonto;
             InterfacePonto.initialize();
             break;
           default:
             throw 'Invalid page';
         };
 
-        if(callback === 'function')
+        if(typeof callback === 'function') // Correção na checagem de função
             callback(pageActive);
     }
 }
