704b719b14dbff5b29c6681da42716b7c776810a
cfsdev027 <cfsdev.github@gmail.com>
2026-05-07 08:56:51 -0300

    Add ServideCookies object for cookie management

--- service_cookies.js
+++ service_cookies.js
@@ -1,1 +1,25 @@
 
+export const ServideCookies = {
+    set: function(nome, valor, dias) {
+        let expiracao = "";
+        if (dias) {
+            const data = new Date();
+            data.setTime(data.getTime() + (dias * 24 * 60 * 60 * 1000));
+            expiracao = "; expires=" + data.toUTCString();
+        }
+        document.cookie = nome + "=" + (valor || "") + expiracao + "; path=/; SameSite=Strict";
+    },
+    get: function(nome) {
+        const nomeEQ = nome + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nomeEQ) == 0) return c.substring(nomeEQ.length, c.length);
+        }
+        return null;
+    },
+    erase: function(nome) {
+        document.cookie = nome + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
+};
