927883e1e9fe26fa42b791a3a489db8d50e6eea8
cfsdev027 <cfsdev.github@gmail.com>
2026-05-07 12:53:34 -0300

    Add Supabase configuration to app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+//import { InterfacePontos } from './interface-pontos.js';
+
 // Configurações do Supabase (Pode deixar exposto no GitHub Pages sem problemas se o RLS estiver ativo)
 const SUPABASE_URL = "https://sdakvoeythnbfqfgupzf.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYWt2b2V5dGhuYmZxZmd1cHpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxMDA1MjksImV4cCI6MjA5MzY3NjUyOX0.U13KCcwWUTStp1-k8at9CudflI66uJ8YhMzSErQAlrM";
