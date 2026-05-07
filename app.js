7175cfac9907893729d41112de938fce737c4e8b
cfsdev027 <cfsdev.github@gmail.com>
2026-05-07 14:57:28 -0300

    Disable self-authentication and page activation
    Comment out authentication logic and imports.

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-import { AppPages } from './app_pages.js';
-import { ServiceAuthentication } from './service_authentication.js';
+//import { AppPages } from './app_pages.js';
+//import { ServiceAuthentication } from './service_authentication.js';
 
 try {
     alert('Carregamento da pagina iniciado...');
-    const selfAuthenticateStatus = await ServiceAuthentication.self_authenticate((data) => {
-        alert('User: ' + JSON.stringify(data));
-    });
+    //const selfAuthenticateStatus = await ServiceAuthentication.self_authenticate((data) => {
+    //   alert('User: ' + JSON.stringify(data));
+    //});
 
-    if(selfAuthenticateStatus) {
-        AppPages.activate('ponto');
-    } else {
-        AppPages.activate('login');
-    }
+    //if(selfAuthenticateStatus) {
+    //    AppPages.activate('ponto');
+    //} else {
+    //    AppPages.activate('login');
+    //}
 } catch(err) {
     alert(err.message);
 }
