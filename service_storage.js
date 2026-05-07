fc2ff926a7012fece76d0de0a8dbf200d5209e43
cfsdev027 <cfsdev.github@gmail.com>
2026-05-07 09:25:37 -0300

    Add ServiceStorage for localStorage management

---
+++ service_storage.js
@@ -0,0 +1,15 @@
+export const ServiceStorage = {
+    set: function(key,data) {
+        localStorage.setItem(key,JSON.stringify(data));
+    },
+    get: function(key){
+        try {
+            return JSON.parse(localStorage.getItem(key))  
+        } catch(e) {
+            return null;
+        };
+    },
+    arrase: function(key) {
+        localStorage.removeItem(key);
+    }
+}
